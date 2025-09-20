876790da316e857cec27a805cd47f714a037b8b1
Dung <88266861+lifehacker5882@users.noreply.github.com>
2025-09-20 17:13:15 +0200

    debug

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,6 +12,11 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log("DEBUG Firebase env:", {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+});
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
